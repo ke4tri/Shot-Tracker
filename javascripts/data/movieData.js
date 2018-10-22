4a1bbf6e2e10059e8857716f1400b8adea981aa2
Wayne Collier <ke4tri@gmail.com>
2018-10-22 17:45:31 -0500

    cleaning

--- javascripts/data/movieData.js
+++ javascripts/data/movieData.js
@@ -1,6 +1,5 @@
 
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_list
-// https://www.w3schools.com/jquery/jquery_filters.asp
+
 
 $(document).ready(function(){
     $("#myInput").on("keyup", function() {
