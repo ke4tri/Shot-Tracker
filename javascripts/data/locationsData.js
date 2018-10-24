712f5f386c5fb5209d3c2f84f5a19047e34f1dfc
Wayne Collier <ke4tri@gmail.com>
2018-10-23 21:10:33 -0500

    rearranged the structure of the movie builder

--- javascripts/data/locationsData.js
+++ javascripts/data/locationsData.js
@@ -1,11 +1,11 @@
 import {locationPrint} from "../data/events.js"
 
-//NEED TO MAKE THE BELOW ONE FUNCTION THAT IS CALLED
-//BASED OFF OF THE ID VALUE OF THE E.TARGET
-//COULD JUST PASS THE E.ID FROM E TO THE FUNCTION AS A PARAMETER?
-
 $('body').on('click', 'button#all', (e) => {
   // HAVE TO ADD THE OTHER 
+    $('.Afternoon').hide();
+    $('.Dark').hide();
+    $('.Evening').hide();
+    $('.Morning').hide();
     locationPrint();
   })
 
