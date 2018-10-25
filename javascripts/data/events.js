cf3a15fde86f979ef1cc190b255615624809a04e
Wayne Collier <ke4tri@gmail.com>
2018-10-24 19:37:17 -0500

    Switched all my events over to promises

--- javascripts/data/events.js
+++ javascripts/data/events.js
@@ -1,26 +1,55 @@
 import {writeMovie} from "../components/movieComponent.js"
 import {writeMovie2} from "../components/locationComponents.js"
 
+
 // Load Location
 
 const locationPrint = () => {
-  $.get('../db/locations.json')
-.done((data) => {
-  writeMovie2(data.locations)
-})
-.fail((error) => {
-  console.error(error);
-}); 
+  return new Promise((resolve, reject)=> {
+      $.get('../db/locations.json')
+    .done((data) => {
+     resolve( writeMovie2(data.locations)) //
+    })
+    .fail((error) => {
+      console.error(error);
+      reject(error)
+    }); 
+    })
+};
+
+locationPrint(); //instead of calling it here put it in a Promise as another .js file
+
+
+const moviePrint = () => {
+  return new Promise((resolve, reject) => {
+      $.get('../db/movie.json')
+        .done((data2) => {
+          resolve(writeMovie(data2.movie))
+      })    
+      .fail((error) => {
+        console.error(error);
+        reject(error)
+      });  
+  })
 }
 
-locationPrint();
-// Load Movie
-$.get('../db/movie.json')
-.done((data2) => {
-  writeMovie(data2.movie);
-})
-.fail((error) => {
-  console.error(error);
-}); 
+// call this on the main.js as the initalizer
+const moviePrintCall = () =>{   
+    moviePrint()
+      .then((data2)=>{
+        console.log("done")
+    })
+    .catch((err)=>{
+        console.error(err);
+    });
+};
+
+export {moviePrintCall}
+
+
+
+
+
+
 
 export {locationPrint};
